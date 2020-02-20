724ce1ac489ca0d95002cce6fa05eea3eb94c28d
Oier Bravo <oier.bravo@biko2.com>
2020-02-20 18:20:59 +0100

    :bug: syntax fix

--- src/twigSvgSprites.php
+++ src/twigSvgSprites.php
@@ -49,7 +49,7 @@ class twigSvgSprites extends \Twig_Extension {
     $default_theme = $theme_handler->getDefault();
     $theme_path = $theme_handler->getTheme($default_theme)->getPath();
     $output = '<svg class="icon ' . $iconClass .' svg--' . $name . '-dims'  .  '">';
-    $output += '<use xlink:href="/' . $theme_path . '/assets/svg/sprite/sprite.svg#' . $name . '"></use></svg>';
+    $output .= '<use xlink:href="/' . $theme_path . '/assets/svg/sprite/sprite.svg#' . $name . '"></use></svg>';
     return $output;
   }
 
